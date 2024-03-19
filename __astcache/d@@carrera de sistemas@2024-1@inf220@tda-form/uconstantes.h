0d102c46c3b207dc1829a7334791246ee9f0b664
GMBismark404 <bismarkaguilar59@gmail.com>
2024-03-18 22:50:31 -0400

    csmemoria 01

---
+++ __astcache/d@@carrera de sistemas@2024-1@inf220@tda-form/uconstantes.h
@@ -0,0 +1,14 @@
+﻿//---------------------------------------------------------------------------
+
+#ifndef UConstantesH
+#define UConstantesH
+//---------------------------------------------------------------------------
+#include <iostream>
+#include <string>
+#include <stdexcept>
+const int MAX = 20;
+const int NULO = -1;
+typedef int direccion;
+typedef int TipoElemento;
+
+#endif
